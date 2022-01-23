77ac42d5b57db6be384e6d14399a930471dacc2b
Åsa Berglund <you@example.com>
2022-01-23 18:24:40 +0100

    commit

---
+++ Moment1/Program.cs
@@ -0,0 +1,10 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddRazorPages();
+var app = builder.Build();
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapRazorPages();
+
+app.Run();
