551de594a42799d33bab44fdedcd6dbd63af2c65
Åsa Berglund <you@example.com>
2022-01-28 10:17:51 +0100

    added js script

--- Moment1/wwwroot/js/main.js
+++ Moment1/wwwroot/js/main.js
@@ -1,1 +1,10 @@
-﻿"use strict"
+﻿
+"use strict"
+
+alert("Hello");
+
+
+// Function to change webpage background color
+function changeBodyBg(color) {
+    document.getElementById("main-container").style.background= color;
+}
